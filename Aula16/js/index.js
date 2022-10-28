7cea060351aaf754b89dd14b9033ddadb21e5a7d
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-10-27 22:00:06 -0300

    .

---
+++ Aula16/js/index.js
@@ -0,0 +1,20 @@
+let alunos = ['luiz', 'maria', 'joao'];
+
+alunos.push('noronha');
+alunos.push('leviatan');
+console.log(alunos instanceof Array);
+
+//delete alunos[2]; deletar elemento 
+//alunos.push('otavio'); adiciona ao final
+//console.log(alunos.slice[0,2]) fatiamento 
+//alunos.unshift('marinho'); 
+//alunos.unshift('noronha');
+//alunos.pop(); remove do final 
+//alunos.shift(); remove do começo 
+//alunos.unshift('fabio') adiciona ao começo 
+//alunos.push('fabia') adiciona ao final 
+//console.log(alunos.length) tamanho de array 
+//alunos[0] = 'Eduardo';
+//alunos[3] = 'luiz'
+//console.log(alunos[0])
+//console.log(alunos[2])
