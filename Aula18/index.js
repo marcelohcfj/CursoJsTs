0f9e717c3454d94d8e0a2ee44639f0925d103157
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-10-28 13:34:38 -0300

    .

---
+++ Aula18/index.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+//function criaPessoa (nome, sobrenome, idade) {
+//    return { nome, sobrenome, idade };
+//}
+
+//const pessoa1 = criaPessoa('Luiz', 'Otavio', 25);
+//const pessoa2 = criaPessoa('Bruna', 'Luz', 19);
+//const pessoa3 = criaPessoa('Flavio', 'Ortega', 42);
+//const pessoa4 = criaPessoa('Andre', 'Luiz', 36);
+//const pessoa5 = criaPessoa('Renato', 'Augusto', 26);
+//console.log(pessoa1.nome, pessoa2.sobrenome)
+
+//const pessoa1 = {
+//    nome: 'Luiz',
+ //   sobrenome: 'Miranda',
+///    idade: 25
+//};
+
+//const pessoa2 = {
+ //   nome: 'Maria',
+ //   sobrenome: 'Oliveira',
+  //  idade: 28
+//};
+
+//console.log(pessoa1.nome);
+
+//const array = [1,2,3];
+//array.push(4);
+//array[0] = 'Luiz';
+//console.log(array);
