229f51f4829d48b9346c15b2fda2d861f2bdd6b8
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-11-03 19:49:18 -0300

    op
    operadores de comparação e lógicos

---
+++ Aula21/index.js
@@ -0,0 +1,16 @@
+/*
+Operadores de comparação 
+> maior que 
+< menor que 
+>= menor que ou igual
+<= menor que ou igual
+== igualdade
+=== igualdade estrita 
+!== diferença estrita 
+!= diferente 
+*/
+
+const num1 = 10;
+const num2 = 11;
+const comp = num1 == num2;
+console.log(comp);
---
+++ Aula22/index.js
@@ -0,0 +1,14 @@
+/*
+Operadores logicos 
+&& -> AND -> E
+\\ -> OR -> OU
+! -> NO -> NÃO
+*/
+
+/* console.log(true && true); */
+
+const usuario = 'Luiz';
+const senha = '123456';
+
+const vailogar = usuario === 'Luiz' && senha === '12345';
+console.log(vailogar);
