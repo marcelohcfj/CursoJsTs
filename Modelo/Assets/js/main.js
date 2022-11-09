258c986685ea07afd3d44f6568590cc80a24340c
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-11-09 13:15:05 -0300

    Update main.js

--- Modelo/Assets/js/main.js
+++ Modelo/Assets/js/main.js
@@ -2,17 +2,43 @@ const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('Evento previnido');
-    setResultado('Olá mundo!');
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        setResultado('Peso Invalido', false);
+        return;
+    } 
+
+    if (!altura) {
+        setResultado('Altura Invalida', false);
+        return;
+    } 
+
+    const imc = getImc(peso, altura);
+
+    console.log(imc);
+
 });
+
+function getImc (peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
 
 function criaP () {
     const p = document.createElement('p');
     return p;
 }
 
-function setResultado (msg) {
+function setResultado (msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
+    
     const p = criaP();
+    p.innerHTML = msg;
+    resultado.appendChild(p);
 }
