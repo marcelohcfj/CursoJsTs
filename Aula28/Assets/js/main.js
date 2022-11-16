b0999bb136de0987ba1ada4b656c90a81b5ee22f
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-11-16 16:05:02 -0300

    Exercício com switch e case
     (concluído(com const))

--- Aula28/Assets/js/main.js
+++ Aula28/Assets/js/main.js
@@ -2,76 +2,13 @@ const h1 = document.querySelector('.container h1');
 const data = new Date();
 
 function getDiaSemanaTexto(diaSemana) {
-    let DiaSemanaTexto;
-
-    switch (diaSemana) {
-        case 0:
-            diaSemanaTexto = 'Domingo';
-            return diaSemanaTexto;
-        case 1:
-            diaSemanaTexto = 'segunda-feira';
-            return diaSemanaTexto;
-        case 2:
-            diaSemanaTexto = 'terça-feira';
-            return diaSemanaTexto;
-        case 3:
-            diaSemanaTexto = 'quarta-feira';
-            return diaSemanaTexto;
-        case 4:
-            diaSemanaTexto = 'quinta-feira';
-            return diaSemanaTexto;
-        case 5:
-            diaSemanaTexto = 'sexta-feira';
-            return diaSemanaTexto;
-        case 6:
-            diaSemanaTexto = 'sábado';
-            return diaSemanaTexto;
-        default:
-            diaSemanaTexto = '';
-    }
+    const diasSemana = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado']; 
+    return diasSemana[diaSemana];
 }
 
 function getNomeMes(numeroMes) {
-    let nomeMes;
-
-    switch (numeroMes) {
-        case 0:
-            nomeMes = 'janeiro';
-            return nomeMes;
-        case 1:
-            nomeMes = 'fevereiro';
-            return nomeMes;
-        case 2:
-            nomeMes = 'março';
-            return nomeMes;
-        case 3:
-            nomeMes = 'abril';
-            return nomeMes;
-        case 4:
-            nomeMes = 'maio';
-            return nomeMes;
-        case 5:
-            nomeMes = 'junho';
-            return nomeMes;
-        case 6:
-            nomeMes = 'julho';
-            return nomeMes;
-        case 7:
-            nomeMes = 'agosto';
-            return nomeMes;
-        case 8:
-            nomeMes = 'setembro';
-            return nomeMes;
-        case 9:
-            nomeMes = 'outubro';
-            return nomeMes;
-        case 10:
-            nomeMes = 'novembro';
-            return nomeMes;
-        case 11:
-            nomeMes = 'dezembro';
-            return nomeMes;
-    }
+    const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+    return meses[numeroMes];
 }
 
 function zeroAEsquerda(num) {
