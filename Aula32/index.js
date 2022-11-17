883961432dc395e8320a41df28521e384ea8e2db
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-11-17 12:27:53 -0300

    for classico

---
+++ Aula32/index.js
@@ -0,0 +1,17 @@
+// console.log('linha0');
+// console.log(linha1);
+// console.log(linha2);
+// console.log(linha3);
+// console.log(linha4);
+// console.log(linha5);
+
+for (let i = 0; i <= 100; i++) {
+    console.log(`Linha ${i}`);
+}
+
+
+//                   (atribuição 10 em 10)
+//                             |
+// for (let i = 0; i <= 100; i =+ 10) {
+//     console.log(`Linha ${i}`);
+// }
