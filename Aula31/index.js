ebc2509e1694830052930ae28c46f9523d4f0ff0
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-11-17 12:04:36 -0300

    atr/dest
    atribuição através de desestruturação (objetos)

---
+++ Aula31/index.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome:  'Marcelo',
+    sobrenome: 'Henrique',
+    idade: 26,
+    endereço: {
+        rua: 'Padre Romildo da Silva',
+        numero: 21,
+    }
+};
+const { nome, sobrenome, idade } = pessoa;
+console.log(nome, sobrenome, idade, 'anos');
+
+
