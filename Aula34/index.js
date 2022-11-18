0b0e4fb8dc7c907b56664dca3013f26adcd210dd
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-11-18 10:45:23 -0300

    For in (objetos)

---
+++ Aula34/index.js
@@ -0,0 +1,25 @@
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Otavio',
+    idade: 26
+};
+
+for (let i in pessoa) {
+    console.log(i, pessoa[i]);
+}
+
+
+
+
+
+
+
+// const  frutas = ['Pera', 'Maça', 'Uva'];
+
+// for (let i in frutas) {
+//     console.log(frutas[i]);
+// }
+
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i]);    
+// }
