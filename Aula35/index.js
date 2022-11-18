6b0dc73928203c99fb629aa838db8ea9f1834b7f
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-11-18 10:58:36 -0300

    for of

---
+++ Aula35/index.js
@@ -0,0 +1,15 @@
+const nome = 'Luiz Otavio';
+
+for (let i of nome) {
+    console.log([i]);
+}
+ 
+
+// for (let i in nome) {
+//     console.log(nome[i]);
+// }
+
+// for (let i = 0; i < nome.length; i++) {
+//     console.log(nome[i]);
+// }
+ 
