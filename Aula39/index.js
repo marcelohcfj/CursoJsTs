b06778f7b69383932d00a252729c22d59afb7bdf
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-11-22 22:32:40 -0300

    ex func

---
+++ Aula39/index.js
@@ -0,0 +1,6 @@
+function max (x, y) {
+    if (x > y)  return x;
+    return y;
+}
+
+console.log(max(10, 2));
---
+++ Aula40/index.js
@@ -0,0 +1,4 @@
+function ePaisagem(largura, altura) {
+    return largura > altura;
+}
+console.log(ePaisagem(1920, 1080));
