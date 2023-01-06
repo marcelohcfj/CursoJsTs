e3448b2c660b59790afa782f4a3b8c9b39296820
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-01-05 21:10:39 -0300

    Lista de tarefas
    Ex

---
+++ Aula46/js/main.js
@@ -0,0 +1,52 @@
+
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+inputTarefa.addEventListener('keypress', function(e){
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    }
+});
+
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'apagar esta tarefa');
+    li.appendChild(botaoApagar);
+}
+
+function criaTarefa(textoInput) {
+    const li = criaLi();
+    li.innerText = textoInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
+}
+
+btnTarefa.addEventListener('click', function(e) {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener('click', function(e){
+    const el = e.target;
+    
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+    }
+
+});
