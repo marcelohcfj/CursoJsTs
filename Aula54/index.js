f0c6b5ecae484bd29c18bf96b2f666c100cf8605
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-01-24 18:46:02 -0300

    Factory Functions

---
+++ Aula54/index.js
@@ -0,0 +1,34 @@
+function criaPessoa(nome, sobrenome, a, p) {
+    return {
+        nome, 
+        sobrenome,
+        // getter
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`;
+        },
+
+        //setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');
+            this.nome = valor.shift();
+            this.sobrenome = valor.join(' ');
+            console.log(valor);
+        },
+
+        fala(assunto) {
+           return `${this.nome} esta ${assunto}.`;
+        },
+        altura: a,
+        peso: p,
+        // getter
+        get imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
+        }
+    };
+}
+
+const p1 = criaPessoa('Marcelo', 'Henrique', 1.70, 68);
+p1.nomeCompleto = 'Marcelo Henrique Costa Ferreira Junior';
+console.log(p1.nome);
+console.log(p1.sobrenome);
