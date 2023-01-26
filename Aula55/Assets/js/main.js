8de76521a6d80a2c371d2b07b3fce129af64baf5
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-01-25 21:29:58 -0300

    Calculadora/factory.function

---
+++ Aula55/Assets/js/main.js
@@ -0,0 +1,6 @@
+<tr>
+    <td><button class="btn btn-clear">C</button></td>
+    <td><button class="btn btn-num">(</button></td>
+    <td><button class="btn btn-num">)</button></td>
+    <td><button class="btn btn-num">/</button></td>
+</tr>
