20325a3de60d4d40be47b87a83b6eac0d673c170
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-10-17 12:34:41 -0300

    Webpack (Boilerplate)

---
+++ Aula89/public/assets/js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// npm i --save-dev @babel/core @babel/cli @babel/presets-env babel-loader webpack webpack-cli regenerator-runtime core-js@2
+
+alert('11111');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ Aula89/src/index.js
@@ -0,0 +1,4 @@
+
+// npm i --save-dev @babel/core @babel/cli @babel/presets-env babel-loader webpack webpack-cli regenerator-runtime core-js@2
+
+alert('11111');
