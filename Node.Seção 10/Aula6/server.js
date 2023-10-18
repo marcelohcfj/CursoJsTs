5719fec2095b251af42e3cfed5ba2ddcebf6615f
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-10-18 15:49:37 -0300

    Express - Introdução

---
+++ Node.Seção 10/Aula6/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+    <form action="/" method="POST">
+    Nome: <input name="" type="text" name="nome">
+    <button>Enviar<button>
+    </form>
+    `);
+})
+
+app.post('/', (req, res) => {
+    res.send('Recebi o formulário')
+});
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado por entrar em contato.')
+});
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000')
+    console.log('Servidor executando na porta 3000');
+});
