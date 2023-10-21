c6d4cce38376024f32c18c02ea8e3659b94e4361
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-10-21 15:16:25 -0300

    Criando usuarios

--- projetoagenda/src/middlewares/middleware.js
+++ projetoagenda/src/middlewares/middleware.js
@@ -1,7 +1,8 @@
 const res = require("express/lib/response");
 
 exports.meuMiddleware = (req, res, next) => {
-    res.locals.umaVariavelLocal = 'Este é o valor da variavel local'; 
+    res.locals.errors = req.flash('errors'); 
+    res.locals.success = req.flash('success'); 
     next();
 }
 
