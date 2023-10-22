0fb0e41e032bfbe082eb16006a47bd20ccfb3dea
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-10-22 15:07:04 -0300

    Cadastrando contatos

--- projetoagenda/routes.js
+++ projetoagenda/routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeControler');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+
+const { loginRequired } = require('./src/middlewares/middleware');
 
 // Rotas da home //
 route.get('/', homeController.index);
@@ -11,6 +14,11 @@ route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+// Rotas de contato //
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
 
 
 module.exports = route;
--- projetoagenda/src/middlewares/middleware.js
+++ projetoagenda/src/middlewares/middleware.js
@@ -18,4 +18,14 @@ exports.checkCsrfError = (err, req, res, next) => {
 exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
-  };
+};
+
+exports.loginRequired = (req, res, next) => {
+    if(!req.session.user) {
+      req.flash('errors', 'Você precisa fazer login.');
+      req.session.save(() => res.redirect('/'));
+      return;
+    }
+  
+    next();
+};
