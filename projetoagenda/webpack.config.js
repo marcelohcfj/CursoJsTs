fb454029894e7a0dcb35b0436b0830dfb79cd727
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-10-23 15:43:25 -0300

    Correção ao apagar contatos

--- projetoagenda/webpack.config.js
+++ projetoagenda/webpack.config.js
@@ -1,26 +1,23 @@
-const path = require('path');
+const path = require('path'); // CommonJS
 
 module.exports = {
-    mode: 'production',
-    entry: './frontend/index.js',
-    output: { 
-        path:    path.resolve(__dirname, 'public', 'assets', 'js'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            exclude: /node_modules/,
-            test: /\.js$/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/env']
-                }
-            }
-        }, {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        }]
-    },
-    devtool: 'source-map'
+  mode: 'production',
+  entry: './frontend/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }]
+  },
+  devtool: 'source-map'
 };
