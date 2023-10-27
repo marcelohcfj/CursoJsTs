c88b197a54df7785afff51cfefe2bf92a67df490
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-10-27 12:49:31 -0300

    CRUD de usuarios

---
+++ api_rest/src/models/User.js
@@ -0,0 +1,57 @@
+// eslint-disable-next-line import/no-extraneous-dependencies, no-unused-vars
+import Sequelize, { Model } from 'sequelize';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 100],
+            msg: 'Campo nome deve ter entre 3 e 255 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email já existe',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Email invalido',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 50],
+            msg: 'A senha deve ter entre 6 e 50 caracteres',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+}
