93ae893c77c9f1005e791dfe279b89b5b2f06801
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-10-27 12:49:21 -0300

    CRUD de usuarios

--- api_rest/src/controllers/UserController.js
+++ api_rest/src/controllers/UserController.js
@@ -4,10 +4,83 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
       // eslint-disable-next-line no-undef
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+  // Show
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+  // update,
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Missing ID '],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User not exist'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+
+      return res.json(novosDados);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // delete
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Missing ID '],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User not exist'],
+        });
+      }
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
