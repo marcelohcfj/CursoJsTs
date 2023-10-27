43a7ca994b929850f9ff4440e167f5400e5b7fe0
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-10-27 17:49:41 -0300

    Crud de alunos

--- api_rest/src/models/Aluno.js
+++ api_rest/src/models/Aluno.js
@@ -4,12 +4,65 @@ import Sequelize, { Model } from 'sequelize';
 export default class Aluno extends Model {
   static init(sequelize) {
     super.init({
-      nome: Sequelize.STRING,
-      sobrenome: Sequelize.STRING,
-      email: Sequelize.STRING,
-      idade: Sequelize.INTEGER,
-      peso: Sequelize.FLOAT,
-      altura: Sequelize.FLOAT,
+      nome: {
+        type: Sequelize.STRING,
+        dafaultValue: '',
+        validate: {
+          len: {
+            args: [3, 100],
+            msg: 'Nome precisa ter entre 3 e 100 caracteres',
+          },
+        },
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        dafaultValue: '',
+        validate: {
+          len: {
+            args: [3, 100],
+            msg: 'Sobrenome precisa ter entre 3 e 100 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        dafaultValue: '',
+        unique: {
+          msg: 'E-mail já existe',
+        },
+        isEmail: {
+          len: {
+            msg: 'E-mail inválido',
+          },
+        },
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        dafaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Idade precisa ser um numero inteiro',
+          },
+        },
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        dafaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Peso precisa ser um numero inteiro ou de ponto flutuante',
+          },
+        },
+      },
+      altura: {
+        type: Sequelize.FLOAT,
+        dafaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Altura precisa ser um numero inteiro ou de ponto flutuante',
+          },
+        },
+      },
     }, {
       sequelize,
     });
--- api_rest/src/routes/AlunoRoutes.js
+++ api_rest/src/routes/AlunoRoutes.js
@@ -1,9 +1,18 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
 // eslint-disable-next-line import/no-extraneous-dependencies, quotes
 import { Router } from "express";
 // eslint-disable-next-line quotes, import/newline-after-import
 import alunoController from "../controllers/AlunoController";
+
+import loginRequired from "../middlewares/loginRequired";
+
 const router = new Router();
 
 router.get('/', alunoController.index);
+router.post('/', loginRequired, alunoController.store);
+router.put('/:id', loginRequired, alunoController.update);
+router.get('/:id', alunoController.show);
+router.delete('/:id', loginRequired, alunoController.delete);
 
 export default router;
