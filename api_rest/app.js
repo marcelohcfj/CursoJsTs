0fc7ccfe7722ce464f56756c686dec4fe33f57d0
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-10-28 16:06:29 -0300

    Arquivos estáticos

--- api_rest/app.js
+++ api_rest/app.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/first */
 import dotenv from 'dotenv';
+import { resolve } from 'path';
 
 dotenv.config();
 
@@ -24,6 +25,8 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
+    this.app.use(express.static(resolve(__dirname, 'uploads')));
+
   }
 
   routes() {
