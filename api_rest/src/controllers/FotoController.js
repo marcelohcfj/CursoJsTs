f4d77ba3d3f2797c6962136df327602866d8a3c0
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-10-28 12:29:44 -0300

    Tratando os arquivos enviados

--- api_rest/src/controllers/FotoController.js
+++ api_rest/src/controllers/FotoController.js
@@ -1,6 +1,20 @@
+// eslint-disable-next-line quotes
+import multer from "multer";
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
--- api_rest/src/routes/fotoRoutes.js
+++ api_rest/src/routes/fotoRoutes.js
@@ -1,15 +1,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies, quotes
 import { Router } from "express";
 // eslint-disable-next-line quotes
-import multer from "multer";
+
 // eslint-disable-next-line quotes, import/newline-after-import
 import fotoController from "../controllers/FotoController";
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('foto'), fotoController.store);
+router.post('/', fotoController.store);
 
 export default router;
