479bb85e3d161f1616cab5e6a65340b2249ffadf
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-11-01 14:24:15 -0300

    Apagando tarefas

--- REACT/listatarefas/src/components/Main.js
+++ REACT/listatarefas/src/components/Main.js
@@ -33,6 +33,20 @@ export default class Main extends Component {
         });
     }
 
+    handleEdit = (e, index) => {
+
+    }
+
+    handleDelete = (e, index) => {
+        const { tarefas } = this.state;
+        const novasTarefas = [ ... tarefas];
+        novasTarefas.splice(index, 1);
+
+        this.setState({
+            tarefas: [ ...novasTarefas],
+        })
+    }
+
     render() {
         const { novaTarefa, tarefas } = this.state;
         
@@ -48,12 +62,12 @@ export default class Main extends Component {
                 </form>
 
                 <ul className="tarefas">
-                    {tarefas.map(tarefa => (
+                    {tarefas.map((tarefa, index) => (
                         <li key={tarefa}>
                             {tarefa}
                             <span>
-                                <FaEdit className="edit" />
-                                <FaWindowClose className="delete" />
+                                <FaEdit onClick={(e) =>this.handleEdit(e, index)} className="edit" />
+                                <FaWindowClose onClick={(e) =>this.handleDelete(e, index)} className="delete" />
                             </span>
                         </li>
                     ))}
