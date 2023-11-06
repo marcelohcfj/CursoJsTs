b9a0193413f1d20f0c63cd9174e1acad94ff1bca
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-11-06 12:59:01 -0300

    React router DOM

--- REACT/reactbase/src/App.js
+++ REACT/reactbase/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Login from './pages/Login';
+import { BrowserRouter } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
+import Routes from '../src/routes';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyles />
-    </>
+    </BrowserRouter>
   );
 }
 
