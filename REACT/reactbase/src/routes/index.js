4ee19ac5d9c189698223c6e0c490325f8284380e
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-11-06 14:42:07 -0300

    React Toastify

--- REACT/reactbase/src/routes/index.js
+++ REACT/reactbase/src/routes/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import MyRoute from './MyRoute';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
+  toast.success('🦄 Toma sua Toast', {
+    toastId: 'sucess',
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
+  });
+
+  toast.error('🦄 Toma seu erro fdp', {
+    toastId: 'error',
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
+  });
   return (
     <Switch>
-      <MyRoute exact path="/" component={Login} isClosed />
+      <MyRoute exact path="/" component={Login} />
       <MyRoute path="*" component={Page404} />
     </Switch>
   );
--- REACT/reactbase/src/styles/GlobalStyles.js
+++ REACT/reactbase/src/styles/GlobalStyles.js
@@ -1,7 +1,13 @@
 import styled, { createGlobalStyle } from 'styled-components';
-import { primaryColor, primaryDarkColor } from '../config/colors';
+import * as colors from '../config/colors';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default createGlobalStyle`
+:root{
+  --toastify-icon-color-success: ${colors.successColor};
+  --toastify-icon-color-error: ${colors.errorColor};
+}
+
 *{
   margin: 0;
   padding: 0;
@@ -11,8 +17,8 @@ export default createGlobalStyle`
 
 body {
   font-family: sans-serif;
-  background: ${primaryDarkColor};
-  color: ${primaryColor};
+  background: ${colors.primaryDarkColor};
+  color: ${colors.primaryColor};
 }
 
 html, body, #root {
@@ -21,7 +27,7 @@ html, body, #root {
 
 button {
   cursor: pointer;
-  background: ${primaryColor};
+  background: ${colors.primaryColor};
   border: none;
   color: #fff;
   padding: 10px 20px;
@@ -31,11 +37,31 @@ button {
 
 a {
   text-decoration: none;
-  color: ${primaryColor};
+  color: ${colors.primaryColor};
 }
 
 ul {
   list-style: none;
+}
+
+body .Toastify .Toastify__toast-container .Toastify__toast--success {
+  background: ${colors.successColor};
+  color: black;
+}
+
+.Toastify__progress-bar--success {
+  background: green;
+}
+
+
+
+body .Toastify .Toastify__toast-container .Toastify__toast--error {
+  background: ${colors.errorColor};
+  color: black;
+}
+
+.Toastify__progress-bar--error {
+  background: red;
 }
 `;
 
