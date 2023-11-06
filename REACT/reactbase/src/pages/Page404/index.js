a836aedb0bb9f687486807bdf2e43895357e352c
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2023-11-06 13:59:29 -0300

    My route

--- REACT/reactbase/src/pages/Page404/index.js
+++ REACT/reactbase/src/pages/Page404/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container } from '../../styles/GlobalStyles';
 
-
 export default function Page404() {
   return (
     <Container>
---
+++ REACT/reactbase/src/routes/MyRoute.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function MyRoute({ component: Component, isClosed, ...rest }) {
+  const isLoggedIn = false;
+
+  if (isClosed && !isLoggedIn) {
+    return (
+      <Redirect
+        to={{ pathname: '/login', state: { prevPath: rest.location.pathname } }}
+      />
+    );
+  }
+
+  return <Route {...rest} component={Component} />;
+}
+
+MyRoute.propTypes = {
+  component: PropTypes.oneOfType.isRequired,
+  isClosed: PropTypes.bool,
+};
+
+MyRoute.defaultProps = {
+  isClosed: false,
+};
