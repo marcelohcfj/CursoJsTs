84ef12c34121ad96c8064c03ea31f06d6c8fc5f2
Marcelo Henrique <marcelosbs07@gmail.com>
2023-11-14 15:48:24 -0300

    Excluindo alunos

--- REACT/consumirapi/src/pages/Alunos/index.js
+++ REACT/consumirapi/src/pages/Alunos/index.js
@@ -3,9 +3,10 @@ import React, { useState } from 'react';
 import { get } from 'lodash'
 import { Link } from 'react-router-dom';
 import { Container } from '../../styles/GlobalStyles';
-import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
+import { FaUserCircle, FaEdit, FaWindowClose, FaExclamation } from 'react-icons/fa';
 import { AlunoContainer, ProfilePicture } from './styled';
 import axios from '../../services/axios';
+import { toast } from 'react-toastify';
 
 export default function Alunos() {
   const [alunos, setAlunos] = useState([]);
@@ -17,6 +18,32 @@ export default function Alunos() {
 
     getData();
   }, []);
+
+  const handleDeleteAsk = e => {
+    e.preventDefault();
+    const exclamation = e.currentTarget.nextSibling;
+    exclamation.setAttribute('display', 'block');
+    e.currentTarget.remove()
+  }
+
+  const handleDelete = async (e, id, index) => {
+    e.persist();
+
+    try {
+      await axios.delete(`/alunos/${id}`);
+      const novosAlunos = [...alunos];
+      novosAlunos.splice(index, 1);
+      setAlunos(novosAlunos);
+    } catch (err) {
+      const status = get(err, 'response.status', 0);
+
+      if (status === 401) {
+        toast.error('Você precisa fazer login');
+      } else {
+        toast.error('Ocorreu um erro ao excluir aluno');
+      }
+    }
+  };
 
   return (
     <Container>
@@ -32,7 +59,9 @@ export default function Alunos() {
             <span>{aluno.email}</span>
 
             <Link to={`/aluno/${aluno.id}/edit`}><FaEdit size={16} /></Link>
-            <Link to={`/aluno/${aluno.id}/edit`}><FaWindowClose size={16} /></Link>
+            <Link onClick={handleDeleteAsk} to={`/aluno/${aluno.id}/delete`}><FaWindowClose size={16} /></Link>
+
+            <FaExclamation size={16} display="none" cursor="pointer" onClick={e => handleDelete(e, aluno.id)} />
           </div>
         ))}
       </AlunoContainer>
