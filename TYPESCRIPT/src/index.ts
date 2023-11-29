1671febedffcdc77d4a6ca9548dd0e45403813a9
Marcelo Henrique <marcelosbs07@gmail.com>
2023-11-29 12:45:06 -0300

    Update index.ts

--- TYPESCRIPT/src/index.ts
+++ TYPESCRIPT/src/index.ts
@@ -2,3 +2,9 @@ const nome = 'Marcelo';
 const sobrenome = 'Henrique';
 
 console.log(nome, sobrenome);
+
+export function exibirFrase(frase: string) {
+  console.log(frase);
+}
+
+exibirFrase('Esta é a frase');
