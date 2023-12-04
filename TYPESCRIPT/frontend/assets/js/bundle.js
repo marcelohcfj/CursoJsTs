2b2893ec44ad2d5d38d969f9164d0b44ac5fb7d7
Marcelo Henrique <marcelosbs07@gmail.com>
2023-12-04 12:08:14 -0300

    exercicio

--- TYPESCRIPT/frontend/assets/js/bundle.js
+++ TYPESCRIPT/frontend/assets/js/bundle.js
@@ -2,14 +2,67 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/webpack/mod.ts":
-/*!****************************!*\
-  !*** ./src/webpack/mod.ts ***!
-  \****************************/
-/***/ (() => {
+/***/ "./src/webpack/index.ts":
+/*!******************************!*\
+  !*** ./src/webpack/index.ts ***!
+  \******************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
 
-console.log('Sou o modulo');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const isEmail_1 = __importDefault(__webpack_require__(Object(function webpackMissingModule() { var e = new Error("Cannot find module 'validator/lib/isEmail'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())));
+const SHOW_ERROR_MESSAGES = 'show-error-message';
+const form = document.querySelector('.form');
+const username = document.querySelector('.username');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const password2 = document.querySelector('.password2');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    hideErrorMessages(this);
+    checkForEmptyFields(username, email, password, password2);
+    checkEmail(email);
+    checkEqualPasswords(password, password2);
+    if (shouldSendForm(this))
+        console.log('FORMULÁRIO ENVIADO');
+});
+function checkForEmptyFields(...inputs) {
+    inputs.forEach((input) => {
+        if (!input.value)
+            showErrorMessage(input, 'Campo não pode ficar vazio');
+    });
+}
+function checkEmail(input) {
+    if (!(0, isEmail_1.default)(input.value))
+        showErrorMessage(input, 'Email inválido');
+}
+function checkEqualPasswords(password, password2) {
+    if (password.value !== password2.value) {
+        showErrorMessage(password, 'Senhas não batem');
+        showErrorMessage(password2, 'Senhas não batem');
+    }
+}
+function hideErrorMessages(form) {
+    form
+        .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+        .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGES));
+}
+function showErrorMessage(input, msg) {
+    const formFields = input.parentElement;
+    const errorMessage = formFields.querySelector('.error-message');
+    errorMessage.innerText = msg;
+    formFields.classList.add(SHOW_ERROR_MESSAGES);
+}
+function shouldSendForm(form) {
+    let send = true;
+    form
+        .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+        .forEach(() => (send = false));
+    return send;
+}
 
 
 /***/ })
@@ -34,26 +87,19 @@ console.log('Sou o modulo');
 /******/ 		};
 /******/ 	
 /******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/ 	
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
-var exports = __webpack_exports__;
-/*!******************************!*\
-  !*** ./src/webpack/index.ts ***!
-  \******************************/
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-__webpack_require__(/*! ./mod */ "./src/webpack/mod.ts");
-
-})();
-
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/webpack/index.ts");
+/******/ 	
 /******/ })()
 ;
 //# sourceMappingURL=bundle.js.map
