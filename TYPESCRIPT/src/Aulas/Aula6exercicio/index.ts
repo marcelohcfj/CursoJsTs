ce8374f9ae56c680c4082d62eaee0ae295365ce2
Marcelo Henrique <marcelosbs07@gmail.com>
2023-12-13 12:33:08 -0300

    Corpo exercicio votation

--- TYPESCRIPT/src/Aulas/Aula6exercicio/index.ts
+++ TYPESCRIPT/src/Aulas/Aula6exercicio/index.ts
@@ -6,4 +6,46 @@ type VotationOption = {
 export class Votation {
   private _votationOptions: VotationOption[] = [];
   constructor(public details: string) {}
+
+  addVotationOption(votationOption: VotationOption): void {
+    this._votationOptions.push(votationOption);
+  }
+
+  vote(votationIndex: number): void {
+    if (!this._votationOptions[votationIndex]) return;
+    this._votationOptions[votationIndex].numberOfvotes += 1;
+  }
+
+  get votationOptions(): VotationOption[] {
+    return this._votationOptions;
+  }
 }
+
+export class VotationApp {
+  private votations: Votation[] = [];
+
+  addVotation(votation: Votation): void {
+    this.votations.push(votation);
+  }
+
+  showVotations(): void {
+    for (const votation of this.votations) {
+      console.log(votation.details);
+      for (const votationOption of votation.votationOptions) {
+        console.log(votationOption.option, votationOption.numberOfvotes);
+      }
+      console.log('###');
+      console.log('');
+    }
+  }
+}
+
+const votation1 = new Votation('Qual a sua linguagem de programação favorita?');
+votation1.addVotationOption({ option: 'Python', numberOfvotes: 0 });
+votation1.addVotationOption({ option: 'JavaScript', numberOfvotes: 0 });
+votation1.addVotationOption({ option: 'TypeScript', numberOfvotes: 0 });
+
+const votationApp = new VotationApp();
+votationApp.addVotation(votation1);
+
+votationApp.showVotations();
